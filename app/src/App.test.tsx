d638a441c666ec5894b4ec57661c54132148ce51
Florian Rupprecht <floruppr@gmail.com>
2023-10-06 11:58:05 -0400

    Fix test

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('nmind link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("NMIND Coding Standards Checklist");
   expect(linkElement).toBeInTheDocument();
 });
